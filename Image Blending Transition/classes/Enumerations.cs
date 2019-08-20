7fb889686e0b061954bb75fd479b25e095e496da
Artjoms Lužnovs <klunics97@gmail.com>
2019-08-20 19:17:15 +0300

    Inirial Commit

---
+++ Image Blending Transition/classes/Enumerations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageBlendingTransition
+{
+	[TypeConverter(typeof(EnumDescriptionTypeConverter))]
+	public enum BlendingModes
+	{
+		[Description("Opacity")]
+		Opacity,
+		[Description("Darken")]
+		Darken,
+		[Description("Multiply")]
+		Multiply,
+		[Description("Lighten")]
+		Lighten,
+		[Description("Screen")]
+		Screen,
+		[Description("Color Burn")]
+		ColorBurn,
+		[Description("Linear Burn")]
+		LinearBurn,
+		[Description("Color Dodge")]
+		ColorDodge,
+		[Description("Linear Dodge (Add)")]
+		LinearDodge,
+		[Description("Overlay")]
+		Overlay,
+		[Description("Soft Light")]
+		SoftLight,
+		[Description("Hard Light")]
+		HardLight,
+		[Description("Difference")]
+		Difference
+	}
+
+	[TypeConverter(typeof(EnumDescriptionTypeConverter))]
+	public enum TransitionModes
+	{
+		[Description("No Transition")]
+		NoTransition,
+		[Description("Wipe (Left to Right)")]
+		WipeLeftToRight,
+		[Description("Wipe (Right to Left)")]
+		WipeRightToLeft,
+		[Description("Wipe (Top to Bottom)")]
+		WipeTopToBottom,
+		[Description("Wipe (Bottom to Top)")]
+		WipeBottomToTop,
+		[Description("Split (Horizontal)")]
+		HorizontalSplit,
+		[Description("Split (Vertical)")]
+		VerticalSplit,
+		[Description("Fade")]
+		Fade,
+		[Description("Rectangle")]
+		Rectangle,
+		[Description("Circle")]
+		Circle
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Image-Blending-Transition
+<h1>Image Processing</h1>
+
+<img src="https://i.imgur.com/CZ9vTjW.png" />
+
+# Features
+- Loading image from file (tested formats are PNG (with and without alpha channel), JPG, JPEG, BMP).
+- Saving image to file.
+- Blending modes:
+  - Opacity (configureable);
+  - Darken;
+  - Multiply;
+  - Lighten;
+  - Screen;
+  - Color Burn;
+  - Linear Burn;
+  - Color Dodge;
+  - Linear Dodge (Add);
+  - Overlay;
+  - Soft Light;
+  - Hard Light;
+  - Difference.
+- Transition modes:
+  - No Transition;
+  - Wipe(Left to Right);
+  - Wipe(Right to Left);
+  - Wipe(Top to Bottom);
+  - Wipe(Bottom to Top);
+  - Split (Horizontal);
+  - Split (Vertical);
+  - Fade;
+  - Rectangle;
+  - Circle.
